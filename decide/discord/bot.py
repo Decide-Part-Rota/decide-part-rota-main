17692c900769d12978005eaf59813307adbcfa91
James Stockwell Mendoza <jstockwellmendoza@gmail.com>
2022-11-23 18:32:32 +0100

    feat: test voting
    Added a dummy test voting and started on the !get_poll [id] command

--- decide/discord/bot.py
+++ decide/discord/bot.py
@@ -7,6 +7,7 @@ from discord.ext import commands
 #TOKEN = os.environ['DISCORD_TOKEN']
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+DEV_MODE = True
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
@@ -26,29 +27,24 @@ async def on_error(event, *args, **kwargs):
 
 @bot.event
 async def on_command_error(ctx, error):
+    message = ""
     if isinstance(error, commands.errors.CommandNotFound):
-        await ctx.send('Invalid command used.')
+        message += 'Invalid command used.'
     elif isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.send('Missing required argument.')
+        message += 'Missing required argument.'
     elif isinstance(error, commands.errors.BadArgument):
-        await ctx.send('Bad argument.')
+        message += 'Bad argument.'
     elif isinstance(error, commands.errors.CommandInvokeError):
-        await ctx.send('Error in command invocation.')
+        message += 'Error in command invocation.'
     elif isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have permission to use this command.')
+        message += 'You do not have permission to use this command.'
     else:
         raise error
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.send('Missing required argument.')
-    elif isinstance(error, commands.errors.BadArgument):
-        await ctx.send('Bad argument.')
-    elif isinstance(error, commands.errors.CommandInvokeError):
-        await ctx.send('Error in command invocation.')
-    else:
-        raise error
+    if DEV_MODE:
+        message += f'\n{error}'
+
+    await ctx.send(message)
 
 ### --- Commands --- ###
 
@@ -73,9 +69,60 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
     ]
     await ctx.send(', '.join(dice))
 
+# Voting
+# - Name
+# - Description
+# - Question
+#   - Description
+#   - Options
+#     - Number
+#     - Option
+# - Start and end date
+
+test_voting = {
+    "name": "this is a test vote",
+    "desc": "this is a test description for a test vote",
+    "question": {
+        "desc": "this is a test description for a test question",
+        "options": {
+            "option0": {
+                "number": 1,
+                "option": "this is a test option"
+            },
+            "option1": {
+                "number": 2,
+                "option": "this is another test option"
+            },
+            "option2": {
+                "number": 3,
+                "option": "this is a third test option"
+            },
+            "option3": {
+                "number": 4,
+                "option": "this is a fourth test option"
+            }
+        }
+    },
+    "start_date": "2021-01-01",
+    "end_date": "2021-01-02"
+}
+
 @bot.command(name='get-poll', help='Gets a poll from ID')
 async def get_poll(ctx, poll_id: int):
     print(f'poll_id: {poll_id}')
     #queryset = Voting.objects.all()
+    embed = discord.Embed(title=f'{test_voting["name"]}', 
+        description=f'{test_voting["desc"]}', color=discord.Color.random())
+
+    embed.add_field(name=f'{test_voting["question"]["desc"]}', value="᲼᲼", inline=False)
+
+    for option in test_voting["question"]["options"]:
+        embed.add_field(name=f'{test_voting["question"]["options"][option]["number"]}', 
+                        value=f'{test_voting["question"]["options"][option]["option"]}', 
+                        inline=False)
+
+
+    await ctx.send(embed=embed)
+    
 
 bot.run(TOKEN)
