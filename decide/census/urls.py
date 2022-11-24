3e03caa83ef243c4e6ad377c88ba8ee9ebe213be
rafestorn <rafaestrada3@gmail.com>
2022-11-24 18:02:36 +0100

    feat: Added Groups (Gender and Age) functionality
    With this functionality you can add groups of voters to an specific voting.
    You can access by census/add/by_group/age and census/add/by_group/gender
    The functionality is not working properly until decide-part-rota-main Rota1-003 issue is completed.

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('add/', views.census_add, name='census_add'),
     path('add/add_to_census', views.add_to_census),
     path('remove/', views.census_remove, name='census_remove'),
-    path('remove/remove_from_census', views.remove_from_census)
+    path('remove/remove_from_census', views.remove_from_census),
+    path('add/by_group/age', views.census_age),
+    path('add/by_group/age/create', views.add_by_age_to_census),
+    path('add/by_group/gender', views.census_gender),
+    path('add/by_group/gender/create', views.add_by_gender_to_census),
 ]
--- decide/census/views.py
+++ decide/census/views.py
@@ -145,4 +145,92 @@ def remove_from_census(request):
         messages.error(request, "You must be a staff member to access this page")
         return HttpResponse(template.render({'remove': True}, request), status=ST_401)
 
+def census_gender(request):
+    if request.user.is_staff:
+        template = loader.get_template("census_gender.html")
+        votings = Voting.objects.all()
+        try: 
+            genders = set(u.gender for u in User.objects.all())
+        except:
+            genders = set()
+        context = {
+            'votings': votings,
+            'genders': genders,
+        }
+        return HttpResponse(template.render(context, request))
+    else:
+        template = loader.get_template("result_page.html")
+        messages.error(request, "You must be a staff member to access this page")
+        return HttpResponse(template.render({}, request), status=ST_401)
 
+def add_by_gender_to_census(request):
+        template = loader.get_template("result_page.html")
+        if request.user.is_staff:
+            voting_id = request.POST['voting-select']
+            genders = request.POST['gender-select']
+            users = User.objects.filter(gender in genders)
+            for user in users:
+                try:
+                    census_by_voting = Census.objects.get(voting_id=voting_id,voter_id=user.id)
+                except Census.DoesNotExist:
+                    census_by_voting = None
+                status_code=404
+                if census_by_voting == None:
+                    census = Census(voting_id=voting_id, voter_id=user.id)
+                    census.save()
+                    messages.success(request, "User added to the voting correctly")
+                    status_code=ST_201
+
+                else:
+                    messages.info(request, "The user was already assigned to the voting")
+                    status_code = 200
+
+                return HttpResponse(template.render({}, request), status=status_code)
+
+        else:
+            messages.error(request, "You must be a staff member to access this page")
+            return HttpResponse(template.render({}, request), status=ST_401)
+
+def census_age(request):
+    if request.user.is_staff:
+        template = loader.get_template("census_age.html")
+        votings = Voting.objects.all()
+        context = {
+            'votings': votings,
+        }
+        return HttpResponse(template.render(context, request))
+    else:
+        template = loader.get_template("result_page.html")
+        messages.error(request, "You must be a staff member to access this page")
+        return HttpResponse(template.render({}, request), status=ST_401)
+
+
+def add_by_age_to_census(request):
+    template = loader.get_template("result_page.html")
+    if request.user.is_staff:
+        voting_id = request.POST['voting-select']
+        minAge = request.POST['minimum-age']
+        maxAge = request.POST['maximum-age']
+        users = User.objects.filter(age >= minAge and age<=maxAge)
+        for user in users:
+            try:
+                census_by_voting = Census.objects.get(voting_id=voting_id,voter_id=user.id)
+            except Census.DoesNotExist:
+                census_by_voting = None
+            status_code=404
+            if census_by_voting == None:
+                census = Census(voting_id=voting_id, voter_id=user.id)
+                census.save()
+                messages.success(request, "User added to the voting correctly")
+                status_code=ST_201
+
+            else:
+                messages.info(request, "The user was already assigned to the voting")
+                status_code = 200
+        
+        
+            return HttpResponse(template.render({}, request), status=status_code)
+
+    else:
+        messages.error(request, "You must be a staff member to access this page")
+        return HttpResponse(template.render({}, request), status=ST_401)
