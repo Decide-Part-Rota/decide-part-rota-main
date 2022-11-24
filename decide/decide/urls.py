a3a2031a876d34c3a4eba250427fc549e29a7dc7
Fraborcar1 <javiborregocaraballo4@gmail.com>
2022-11-24 20:40:49 +0100

    feat: logged user interface added
    Added the interface another group will use to implements their methods, between then, the user log in.

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
+from visualizer import views
 
 
 schema_view = get_swagger_view(title='Decide API')
@@ -24,6 +25,7 @@ schema_view = get_swagger_view(title='Decide API')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('doc/', schema_view),
+    path('inicio/', views.inicio),
     path('gateway/', include('gateway.urls')),
 ]
 
