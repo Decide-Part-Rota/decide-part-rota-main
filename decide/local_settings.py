93eb2b0d633cdec965c8ccc57da456b44516a0db
joseluisps21 <joseluissfc4@gmail.com>
2022-12-06 19:17:16 +0100

    fix: Census by Group (Maritual Status and Nationality) integration
    In this commits some bugs have been corrected in order to satisfy the requirement.
    Now the functionality is totally working and you can add or remove users to/from
    a voting census.

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -36,10 +36,10 @@ APIS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'decide19',
         'USER': 'decide',
-        'PASSWORD': 'decide',
-        'HOST': '127.0.0.1',
+        'PASSWORD': 'complex',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
