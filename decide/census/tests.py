a71c80e45507dcafb3b5d5d46e9dbb3b0c82916b
vicsotber <victor.soto.berenguer123@gmail.com>
2022-12-09 19:36:50 +0100

    test: Implemented removing from census view test
    A test case has been implemented to check the removing from census functionality using Selenium.

--- decide/census/tests.py
+++ decide/census/tests.py
@@ -407,3 +407,40 @@ class ViewTestCase(StaticLiveServerTestCase):
 
         message = self.driver.find_element(By.TAG_NAME,"ul").find_element(By.TAG_NAME,"li").text
         self.assertEqual(message, "User added to the voting correctly")
+
+    def test_delete_census_from_gui(self):
+        response = self.driver.get(f'{self.live_server_url}/census/remove/')
+        message = self.driver.find_element(By.TAG_NAME,"ul").find_element(By.TAG_NAME,"li").text
+        self.assertEqual(message, "You must be a staff member to access this page")
+
+        self.driver.get(f'{self.live_server_url}/admin/')
+        self.driver.find_element(By.ID,'id_username').send_keys("admin")
+        self.driver.find_element(By.ID,'id_password').send_keys("qwerty",Keys.ENTER)
+
+        response = self.driver.get(f'{self.live_server_url}/census/remove/')
+        dropdown = self.driver.find_element(By.ID, "voting-select")
+        dropdown.find_element(By.XPATH, "//option[. = 'test voting']").click()
+        element = self.driver.find_element(By.ID, "voting-select")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).click_and_hold().perform()
+        element = self.driver.find_element(By.ID, "voting-select")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.ID, "voting-select")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).release().perform()
+        dropdown = self.driver.find_element(By.ID, "user-select")
+        dropdown.find_element(By.XPATH, "//option[. = 'test_user']").click()
+        element = self.driver.find_element(By.ID, "user-select")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).click_and_hold().perform()
+        element = self.driver.find_element(By.ID, "user-select")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.ID, "user-select")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).release().perform()
+        self.driver.find_element(By.CSS_SELECTOR, ".col > .btn").click()
+
+        message = self.driver.find_element(By.TAG_NAME,"ul").find_element(By.TAG_NAME,"li").text
+        self.assertEqual(message, "User removed from the voting correctly")
