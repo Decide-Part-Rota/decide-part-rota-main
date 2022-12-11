fa23b983304ac6e4d72910250a95aeb575ba3415
Fraborcar1 <fraborcar1@alum.us.es>
2022-12-11 09:07:04 +0100

    bug: Codacy again
    Minor css bug hopes fixed for Codacy

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -20,7 +20,7 @@ class VisualizerView(TemplateView):
         try:
             r = mods.get('voting', params={'id': vid})
             context['voting'] = json.dumps(r[0])
-        except: 
+        except:
             raise Http404
 
         return context
