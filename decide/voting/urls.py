15553de62eb61296c8ba7ad61a638401d8ed9b48
Pablo José Robledo Campa <pablorcmail@gmail.com>
2022-12-12 12:27:50 +0100

    fix: Fixed the merge from Rota1-032 manually
    The changes have to be done again because of a problem with the orginal changes are from the developers involved in Rota1-032

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('listadoVotaciones', views.VotacionList.mostrarVotacionesPublicas),
     path('', views.VotingView.as_view(), name='voting'),
     path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
 ]
