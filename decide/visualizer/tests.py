55c3e01cfde94a2ff26e70195a3577a3e0adf6d5
Fraborcar1 <fraborcar1@alum.us.es>
2022-12-13 00:27:45 +0100

    fix: Fixed tests for graphics
    Test for graphics now works again

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,3 +1,4 @@
+import random
 from django.utils import timezone
 from django.conf import settings
 from django.test import TestCase
@@ -8,7 +9,10 @@ from mixnet.models import Auth
 from voting.models import Voting, Question, QuestionOption
 from census.models import Census
 from django.contrib.auth.models import User
+from mixnet.mixcrypt import ElGamal
+from mixnet.mixcrypt import MixCrypt
 
+from base import mods
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -33,35 +37,74 @@ class GraphicsTestCases(BaseTestCase):
         a.save()
         v.auths.add(a)
         #Creamos usuarios para la votación
-        for i in range(25):
+        for i in range(10):
             u, _ = User.objects.get_or_create(username='testvoter{}'.format(i))
             u.is_active = True
             u.save()
             c = Census(voter_id=u.id, voting_id=v.id)
             c.save()
-        user, _ = User.objects.get_or_create(pk=v.pk)
-        user.username = 'user{}'.format(v.pk)
-        user.set_password('qwerty')
-        user.save()
         super().setUp()
+
 
     def tearDown(self):
         super().tearDown()
+    
+    def encrypt_msg(self, msg, v, bits=settings.KEYBITS):
+        pk = v.pub_key
+        p, g, y = (pk.p, pk.g, pk.y)
+        k = MixCrypt(bits=bits)
+        k.k = ElGamal.construct((p, g, y))
+        return k.encrypt(msg)
+    
+    def get_or_create_user(self, pk):
+        user, _ = User.objects.get_or_create(pk=pk)
+        user.username = 'user{}'.format(pk)
+        user.set_password('qwerty')
+        user.save()
+        return user
+
+    def store_votes(self, v):
+        voters = list(Census.objects.filter(voting_id=v.id))
+        voter = voters.pop()
+        
+        clear = {}
+        for opt in v.question.options.all():
+            clear[opt.number] = 0
+            for i in range(random.randint(0, 3)):
+                a, b = self.encrypt_msg(opt.number, v)
+                data = {
+                    'voting': v.id,
+                    'voter': voter.voter_id,
+                    'vote': { 'a': a, 'b': b },
+                }
+                clear[opt.number] += 1
+                user = self.get_or_create_user(voter.voter_id)
+                self.login(user=user.username)
+                voter = voters.pop()
+                mods.post('store', json=data)
+        return clear
 
     def test_correct_access_graphics(self):
         v = Voting.objects.get(name='Helado')
+        v.create_pubkey()
         v.start_date = timezone.now()
         v.save()
+
+        self.store_votes(v)
         self.login()  # set token
         v.tally_votes(self.token)
+
         #Se comprueba que se puede acceder a las gráficas de dicha votación
         response = self.client.post('/graphics/{}'.format(v.pk), format='json')
         self.assertEquals(response.status_code,200)
 
     def test_graphic_template_correct(self):
         v = Voting.objects.get(name='Helado')
+        v.create_pubkey()
         v.start_date = timezone.now()
         v.save()
+
+        self.store_votes(v)
         self.login()  # set token
         v.tally_votes(self.token)
 
@@ -90,18 +133,68 @@ class SeleniumGraphics(StaticLiveServerTestCase):
                                           defaults={'me': True, 'name': 'test auth'})
         a.save()
         v.auths.add(a)
+        #Creamos usuarios para la votación
+        for i in range(10):
+            u, _ = User.objects.get_or_create(username='testvoter{}'.format(i))
+            u.is_active = True
+            u.save()
+            c = Census(voter_id=u.id, voting_id=v.id)
+            c.save()
         super().setUp()
 
     def tearDown(self):
         super().tearDown()
         self.driver.quit()
         self.base.tearDown()
+    
+    def encrypt_msg(self, msg, v, bits=settings.KEYBITS):
+        pk = v.pub_key
+        p, g, y = (pk.p, pk.g, pk.y)
+        k = MixCrypt(bits=bits)
+        k.k = ElGamal.construct((p, g, y))
+        return k.encrypt(msg)
+    
+    def get_or_create_user(self, pk):
+        user, _ = User.objects.get_or_create(pk=pk)
+        user.username = 'user{}'.format(pk)
+        user.set_password('qwerty')
+        user.save()
+        return user
 
-    def test_exist_graphic_bars(self):
+    def store_votes(self, v):
+        voters = list(Census.objects.filter(voting_id=v.id))
+        voter = voters.pop()
+
+        clear = {}
+        for opt in v.question.options.all():
+            clear[opt.number] = 0
+            for i in range(random.randint(0, 3)):
+                a, b = self.encrypt_msg(opt.number, v)
+                data = {
+                    'voting': v.id,
+                    'voter': voter.voter_id,
+                    'vote': { 'a': a, 'b': b },
+                }
+                clear[opt.number] += 1
+                user = self.get_or_create_user(voter.voter_id)
+                self.base.login(user=user.username)
+                voter = voters.pop()
+                mods.post('store', json=data)
+        return clear
+
+    def test_exist_graphics(self):
         v = Voting.objects.get(name='Helado')
+        v.create_pubkey()
+        v.start_date = timezone.now()
+        v.save()
+        self.store_votes(v)
+        self.base.login()
+        v.tally_votes(self.base.token)
         self.driver.get('{}/graphics/{}'.format(self.live_server_url, v.pk))
         tituloPag = self.driver.find_element(By.XPATH, '//div[@class="logo"]').text
         graficaBarras = self.driver.find_elements(By.ID, 'grafic')
+        graficaPie = self.driver.find_elements(By.ID, 'grafica')
         self.assertTrue(len(graficaBarras)==1)
+        self.assertTrue(len(graficaPie)==1)
         self.assertEquals(tituloPag, "Votaciones para 'Helado'")
-      
+
