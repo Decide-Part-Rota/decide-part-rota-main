00601b58421b24cc4682b0fecb358c27f6badbed
Alejandro Gestoso <alegestor@alum.us.es>
2022-12-14 19:21:04 +0100

    Fix: codacy issues (again)

--- decide/authentication/test_selenium_login.py
+++ decide/authentication/test_selenium_login.py
@@ -12,74 +12,71 @@ import time
 
 class LoginTestCase(StaticLiveServerTestCase):
     
-        def setUp(self):
-            self.base = BaseTestCase()
-            self.base.setUp()
+    def setUp(self):
+        self.base = BaseTestCase()
+        self.base.setUp()
+        self.client = APIClient()
+        mods.mock_query(self.client)
+        u123 = User(username='user123')
+        u123.set_password('user123')
+        u123.is_superuser = True
+        u123.save()
+        options = webdriver.ChromeOptions()
+        #Si se quieren ver en navegador, cambiar a false
+        options.headless = True
+        self.driver = webdriver.Chrome(options=options) 
 
-            self.client = APIClient()
-            mods.mock_query(self.client)
-            u123 = User(username='user123')
-            u123.set_password('user123')
-            u123.is_superuser = True
-            u123.save()
+    def tearDown(self):
+        super().tearDown() 
+        self.driver.quit() 
+        self.base.tearDown() 
 
-            options = webdriver.ChromeOptions()
-            #Si se quieren ver en navegador, cambiar a false
-            options.headless = True
+    def test_login_ok(self):
+        self.driver.maximize_window()
+        self.driver.get(''f'{self.live_server_url}''/authentication/accounts/login/?next=/') 
+        self.driver.find_element(By.NAME, "username").clear()
+        self.driver.find_element(By.NAME, "username").send_keys('user123')
+        time.sleep(0.5)
+        self.driver.find_element(By.NAME, "password").clear()
+        self.driver.find_element(By.NAME, "password").send_keys('user123')
+        time.sleep(0.5)
+        self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
+        time.sleep(0.5)
 
-            self.driver = webdriver.Chrome(options=options) 
-    
-        def tearDown(self):
-            super().tearDown() 
-            self.driver.quit() 
-            self.base.tearDown() 
+        self.assertTrue("¡Bienvenido!" in self.driver.page_source)
 
-        def test_login_ok(self):
-            self.driver.maximize_window()
-            self.driver.get(''f'{self.live_server_url}''/authentication/accounts/login/?next=/') 
-            self.driver.find_element(By.NAME, "username").clear()
-            self.driver.find_element(By.NAME, "username").send_keys('user123')
-            time.sleep(0.5)
-            self.driver.find_element(By.NAME, "password").clear()
-            self.driver.find_element(By.NAME, "password").send_keys('user123')
-            time.sleep(0.5)
-            self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
-            time.sleep(0.5)
+    def test_login_fail_wrong_User_Password(self):
+        self.driver.maximize_window()
+        self.driver.get(''f'{self.live_server_url}''/authentication/accounts/login/?next=/') 
+        self.driver.find_element(By.NAME, "username").clear()
+        self.driver.find_element(By.NAME, "username").send_keys('userFail')
+        time.sleep(0.5)
+        self.driver.find_element(By.NAME, "password").clear()
+        self.driver.find_element(By.NAME, "password").send_keys('passwordFail')
+        time.sleep(0.5)
+        self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
+        time.sleep(0.5)
 
-            self.assertTrue("¡Bienvenido!" in self.driver.page_source)
+        self.assertTrue("Inicia sesión" in self.driver.page_source)
 
-        def test_login_fail_wrong_User_Password(self):
-            self.driver.maximize_window()
-            self.driver.get(''f'{self.live_server_url}''/authentication/accounts/login/?next=/') 
-            self.driver.find_element(By.NAME, "username").clear()
-            self.driver.find_element(By.NAME, "username").send_keys('userFail')
-            time.sleep(0.5)
-            self.driver.find_element(By.NAME, "password").clear()
-            self.driver.find_element(By.NAME, "password").send_keys('passwordFail')
-            time.sleep(0.5)
-            self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
-            time.sleep(0.5)
+    def test_login_fail_blank_user(self):
+        self.driver.maximize_window()
+        self.driver.get(''f'{self.live_server_url}''/authentication/accounts/login/?next=/') 
+        self.driver.find_element(By.NAME, "password").clear()
+        self.driver.find_element(By.NAME, "password").send_keys('passwordFail')
+        time.sleep(0.5)
+        self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
+        time.sleep(0.5)
 
-            self.assertTrue("Inicia sesión" in self.driver.page_source)
+        self.assertTrue("Inicia sesión" in self.driver.page_source)
 
-        def test_login_fail_blank_user(self):
-            self.driver.maximize_window()
-            self.driver.get(''f'{self.live_server_url}''/authentication/accounts/login/?next=/') 
-            self.driver.find_element(By.NAME, "password").clear()
-            self.driver.find_element(By.NAME, "password").send_keys('passwordFail')
-            time.sleep(0.5)
-            self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
-            time.sleep(0.5)
+    def test_login_fail_blank_password(self):
+        self.driver.maximize_window()
+        self.driver.get(''f'{self.live_server_url}''/authentication/accounts/login/?next=/') 
+        self.driver.find_element(By.NAME, "username").clear()
+        self.driver.find_element(By.NAME, "username").send_keys('userFail')
+        time.sleep(0.5)
+        self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
+        time.sleep(0.5)
 
-            self.assertTrue("Inicia sesión" in self.driver.page_source)
-
-        def test_login_fail_blank_password(self):
-            self.driver.maximize_window()
-            self.driver.get(''f'{self.live_server_url}''/authentication/accounts/login/?next=/') 
-            self.driver.find_element(By.NAME, "username").clear()
-            self.driver.find_element(By.NAME, "username").send_keys('userFail')
-            time.sleep(0.5)
-            self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
-            time.sleep(0.5)
-
-            self.assertTrue("Inicia sesión" in self.driver.page_source)
+        self.assertTrue("Inicia sesión" in self.driver.page_source)
--- decide/authentication/test_selenium_register.py
+++ decide/authentication/test_selenium_register.py
@@ -5,81 +5,77 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-from unittest import skip
-
 import time 
 
 class RegisterTestCase(StaticLiveServerTestCase):
+      
+    def setUp(self):
+        self.base = BaseTestCase()
+        self.base.setUp()
+        options = webdriver.ChromeOptions()
+        #Si se quieren ver en navegador, cambiar a false
+        options.headless = True
+        self.driver = webdriver.Chrome(options=options) 
     
-        def setUp(self):
-            self.base = BaseTestCase()
-            self.base.setUp()
+    def tearDown(self):
+        super().tearDown() 
+        self.driver.quit() 
+        self.base.tearDown() 
 
-            options = webdriver.ChromeOptions()
-            #Si se quieren ver en navegador, cambiar a false
-            options.headless = True
+    def test_register_ok(self):
+        self.driver.maximize_window()
+        self.driver.get(''f'{self.live_server_url}''/authentication/registerForm/') 
+        self.driver.find_element(By.ID, "id_username").clear()
+        self.driver.find_element(By.ID, "id_username").send_keys("user")
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_password1").clear()
+        self.driver.find_element(By.ID, "id_password1").send_keys("12user34")
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_password2").clear()
+        self.driver.find_element(By.ID, "id_password2").send_keys("12user34")
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_email").clear()
+        self.driver.find_element(By.ID, "id_email").send_keys("user@user.com")
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_sex").find_element(By.XPATH, "//option[. = 'Hombre']").click()
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_age").clear()
+        self.driver.find_element(By.ID, "id_age").send_keys("34")
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_status").find_element(By.XPATH, "//option[. = 'Conviviente']").click()
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_country").find_element(By.XPATH, "//option[. = 'Azerbaijan']").click()
+        time.sleep(0.5)
+        self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
+        time.sleep(0.5)
 
-            self.driver = webdriver.Chrome(options=options) 
-    
-        def tearDown(self):
-            super().tearDown() 
-            self.driver.quit() 
-            self.base.tearDown() 
+        self.assertTrue("Inicia sesión" in self.driver.page_source)
 
-        def test_register_ok(self):
-            self.driver.maximize_window()
-            self.driver.get(''f'{self.live_server_url}''/authentication/registerForm/') 
-            self.driver.find_element(By.ID, "id_username").clear()
-            self.driver.find_element(By.ID, "id_username").send_keys("user")
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_password1").clear()
-            self.driver.find_element(By.ID, "id_password1").send_keys("12user34")
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_password2").clear()
-            self.driver.find_element(By.ID, "id_password2").send_keys("12user34")
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_email").clear()
-            self.driver.find_element(By.ID, "id_email").send_keys("user@user.com")
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_sex").find_element(By.XPATH, "//option[. = 'Hombre']").click()
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_age").clear()
-            self.driver.find_element(By.ID, "id_age").send_keys("34")
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_status").find_element(By.XPATH, "//option[. = 'Conviviente']").click()
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_country").find_element(By.XPATH, "//option[. = 'Azerbaijan']").click()
-            time.sleep(0.5)
-            self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
-            time.sleep(0.5)
-
-            self.assertTrue("Inicia sesión" in self.driver.page_source)
-
-        def test_register_fail_username_wrong_character(self):
-            self.driver.maximize_window()
-            self.driver.get(''f'{self.live_server_url}''/authentication/registerForm/') 
-            self.driver.find_element(By.ID, "id_username").clear()
-            self.driver.find_element(By.ID, "id_username").send_keys("user,")
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_password1").clear()
-            self.driver.find_element(By.ID, "id_password1").send_keys("12user34")
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_password2").clear()
-            self.driver.find_element(By.ID, "id_password2").send_keys("12user34")
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_email").clear()
-            self.driver.find_element(By.ID, "id_email").send_keys("user@user.com")
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_sex").find_element(By.XPATH, "//option[. = 'Hombre']").click()
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_age").clear()
-            self.driver.find_element(By.ID, "id_age").send_keys("34")
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_status").find_element(By.XPATH, "//option[. = 'Conviviente']").click()
-            time.sleep(0.5)
-            self.driver.find_element(By.ID, "id_country").find_element(By.XPATH, "//option[. = 'Azerbaijan']").click()
-            time.sleep(0.5)
-            self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
-            time.sleep(0.5)
+    def test_register_fail_username_wrong_character(self):
+        self.driver.maximize_window()
+        self.driver.get(''f'{self.live_server_url}''/authentication/registerForm/') 
+        self.driver.find_element(By.ID, "id_username").clear()
+        self.driver.find_element(By.ID, "id_username").send_keys("user,")
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_password1").clear()
+        self.driver.find_element(By.ID, "id_password1").send_keys("12user34")
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_password2").clear()
+        self.driver.find_element(By.ID, "id_password2").send_keys("12user34")
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_email").clear()
+        self.driver.find_element(By.ID, "id_email").send_keys("user@user.com")
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_sex").find_element(By.XPATH, "//option[. = 'Hombre']").click()
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_age").clear()
+        self.driver.find_element(By.ID, "id_age").send_keys("34")
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_status").find_element(By.XPATH, "//option[. = 'Conviviente']").click()
+        time.sleep(0.5)
+        self.driver.find_element(By.ID, "id_country").find_element(By.XPATH, "//option[. = 'Azerbaijan']").click()
+        time.sleep(0.5)
+        self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
+        time.sleep(0.5)
             
-            self.assertTrue("Registro" in self.driver.page_source)
+        self.assertTrue("Registro" in self.driver.page_source)
