2404a5f496bebcd17eaac38ca554e1ed96d43a8c
javiervaz01 <javivaz2001@gmail.com>
2022-12-15 01:32:03 +0100

    Changes on how to take voting data to work with it in react, almost working

--- decide/booth/static/crypto/contenedor.js
+++ decide/booth/static/crypto/contenedor.js
@@ -1,22 +1,43 @@
 'use strict';
 
 const create = React.createElement;
-const MyReactComponent = (props) => {
-  const { message } = props;
-  // Use the message prop in your component here
-};
+const datos = document.getElementById("hide").value;
+const dicc = JSON.parse(voting);
+
+//const view = [];
+const keys = Object.keys(dicc);
+
+const id = keys[0];
+const lista = dicc[0];
+const nombre = lista[1];
+const desc = lista[2];
+
+
+
+  //view.push(aux)
+
 
 
 class Contenedor extends React.Component {
-  
 
   render() {
 
 
-    return React.createElement('h1', message);
+    return create("body", {}, 
+      create("fiv",{key: "id", className: "flip-card"},
+      create("td",{key: "name", className: "flip-card-inner"},
+      create("td",{key: "desc", className: "flip-card-front"},nombre),
+      create("div", {className: "flip-card-back"},
+      create('h3',{className:'Logo'},desc)))),
+
+
+);
+    
   }
 }
 
-const domContainer = document.querySelector('#contenedor');
+
+
+const domContainer = document.querySelector('#caja');
 const root = ReactDOM.createRoot(domContainer);
 root.render(create(Contenedor));
